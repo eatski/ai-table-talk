0ca64f20698a30aa8ca97d48889dec39ce2c6d7e
hagaitski <i.haga629@gmail.com>
2024-08-23 06:50:20 +0900

    interval

--- src/pages/api/sse.ts
+++ src/pages/api/sse.ts
@@ -2,24 +2,20 @@ export const config = {
   runtime: "edge",
 };
 
-const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
-
 export default async function handler() {
   const encoder = new TextEncoder();
 
   const readable = new ReadableStream({
     async start(controller) {
-      controller.enqueue(encoder.encode("<html><body>"));
-      await delay(500);
-      controller.enqueue(encoder.encode("<ul><li>List Item 1</li>"));
-      await delay(500);
-      controller.enqueue(encoder.encode("<li>List Item 2</li>"));
-      await delay(500);
-      controller.enqueue(encoder.encode("<li>List Item 3</li></ul>"));
-      await delay(500);
-      controller.enqueue(encoder.encode("</body></html>"));
-      controller.close();
-    },
+     const id = setInterval(() => {
+        controller.enqueue(new Date().toISOString());
+      }, 500)
+      setTimeout(() => {
+        clearInterval(id)
+        controller.close();
+      },60 * 1000 * 10)
+    }
+
   });
 
   return new Response(readable, {
